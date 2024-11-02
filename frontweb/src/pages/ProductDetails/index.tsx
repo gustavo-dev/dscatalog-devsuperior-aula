2b59c075067973d3164563397b35c73e04f5e943
Gustavo Henrique dos Santos <92273898+gustavo-dev@users.noreply.github.com>
2024-11-02 17:29:48 -0300

    Implemented ProductDetails in mobile layout

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,46 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css';
+
 const ProductDetails = () => {
-  return <h1>Página ProductDetails</h1>;
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do Produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>
+                Nome do Produto
+                <ProductPrice price={2345.67} />
+              </h1>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                consectetur, ex vel consectetur consectetur, nunc velit aliquet
+                massa, non consectetur lectus ex id neque. Donec mollis, diam
+                vel aliquet consectetur, ligula neque venenatis nunc, a
+                sollicitudin ligula justo vel nisi. Aliquam erat volutpat.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
