7ebc8aed5838af580934c876ce2d6c7ea6fde148
Gustavo Henrique dos Santos <92273898+gustavo-dev@users.noreply.github.com>
2025-01-19 16:41:45 -0300

    Addding useParams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,20 +1,26 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 import './styles.css';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import axios from 'axios';
 import { BASE_URL } from 'util/requests';
 import { useEffect, useState } from 'react';
 
+type UrlParams = {
+  productId: string;
+};
+
 const ProductDetails = () => {
+  const { productId } = useParams<UrlParams>();
+
   const [product, setProduct] = useState<Product>();
 
   useEffect(() => {
-    axios.get(BASE_URL + '/products/2').then((response) => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
       setProduct(response.data);
     });
-  }, []);
+  }, [productId]);
 
   return (
     <div className="product-details-container">
